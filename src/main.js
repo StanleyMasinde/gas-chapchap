6426a3842231eaa6b763e40443911cadfe378e71
Stanley Masinde <stanleyloren@gmail.com>
2019-08-12 19:20:25 +0300

    added the PWA plugin

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
